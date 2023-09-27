0d061dbc40df8986525efd5a4e55e71ca017ae41
Luis Antonio Martinez Marroquin <LuisMartinezMA12@gmail.com>
2023-09-26 21:44:58 -0600

    funciona

---
+++ Models/scenes/game.go
@@ -0,0 +1,422 @@
+package scenes
+
+import (
+	"fmt"
+	"image"
+	_ "image/jpeg"
+	_ "image/png"
+	"log"
+	"math/rand"
+	"os"
+	"path/filepath"
+	"sync"
+	"time"
+	"github.com/faiface/pixel/pixelgl"
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/text"
+	"golang.org/x/image/colornames"
+	"golang.org/x/image/font/basicfont"
+)
+type Bone struct {
+	X, Y float64
+}
+
+type Dog struct {
+	X, Y, Vy float64
+	Sprite   *pixel.Sprite
+}
+
+type Bird struct {
+	X, Y       float64
+	Frame      int
+	Frames     []*pixel.Sprite
+	CurrentPic int
+	Direction  int // 1 para derecha, -1 para izquierda
+}
+
+var mu sync.Mutex
+var gameEnded bool = false
+var gameWon bool = false
+var walkingFrame int = 1
+var frameCounter int = 0
+var winMessageShown bool = false
+
+const tiempoMaximo = 2045 // Tiempo máximo en segundos (3 minutos)
+var tiempoTranscurrido int
+
+func loadBirdSprites() ([]*pixel.Sprite, error) {
+	var birdSprites []*pixel.Sprite
+
+
+	// Ruta a la carpeta de assets
+	assetsDir := "assets"
+
+
+	// Función para cargar una imagen y convertirla en un pixel.Picture
+	loadPicture := func(path string) (pixel.Picture, error) {
+		file, err := os.Open(path)
+		if err != nil {
+			return nil, err
+		}
+		defer file.Close()
+		img, _, err := image.Decode(file)
+		if err != nil {
+			return nil, err
+		}
+		return pixel.PictureDataFromImage(img), nil
+	}
+
+
+	err := filepath.Walk(assetsDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			// Si es un archivo en lugar de un directorio, intenta cargarlo como una imagen
+			pic, err := loadPicture(path)
+			if err != nil {
+				// Manejar el error si no se puede cargar la imagen
+				log.Printf("Error cargando imagen %s: %v\n", path, err)
+			} else {
+				// Crear un Sprite y agregarlo a la lista de Sprites de pájaros
+				sprite := pixel.NewSprite(pic, pic.Bounds())
+				birdSprites = append(birdSprites, sprite)
+			}
+		}
+		return nil
+	})
+
+	
+	if err != nil {
+		return nil, err
+	}
+
+
+	return birdSprites, nil
+
+}
+
+func loadPicture(path string) (pixel.Picture, error) {
+
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+	img, _, err := image.Decode(file)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return pixel.PictureDataFromImage(img), nil
+
+}
+
+func setDogSprite(dog *Dog, spriteMap map[string]*pixel.Sprite, state string) {
+
+	switch state {
+
+	case "Right1":
+		dog.Sprite = spriteMap["dog2"]
+	case "Right2":
+		dog.Sprite = spriteMap["dog3"]
+	case "Left1":
+		dog.Sprite = spriteMap["dog5"]
+	case "Left2":
+		dog.Sprite = spriteMap["dog6"]
+	case "JumpingRight":
+		dog.Sprite = spriteMap["dog7"]
+	case "JumpingLeft":
+		dog.Sprite = spriteMap["dog8"]
+	case "NeutralLeft":
+		dog.Sprite = spriteMap["dog4"]
+	default:
+		dog.Sprite = spriteMap["dog1"]
+	}
+
+}
+// ... Resto del código relacionado con el juego ...
+
+func Aa() {
+
+	cfg := pixelgl.WindowConfig{
+
+		Title:  "Juego del perrito",
+		Bounds: pixel.R(0, 0, 800, 600),
+		
+	}
+
+	win, err := pixelgl.NewWindow(cfg)
+
+	if err != nil {
+
+		panic(err)
+
+	}
+
+	var wg sync.WaitGroup
+
+	birdSprites, err := loadBirdSprites()
+
+	if err != nil {
+
+		log.Fatalf("Error cargando imágenes de pájaros: %v", err)
+		
+	}
+
+	bird := Bird{
+		X:         rand.Float64() * win.Bounds().Max.X,
+		Y:         win.Bounds().Max.Y - 50, // Ajusta la posición vertical
+		Direction: 1,                       // Empieza volando hacia la derecha
+	}
+
+	bird.Frames = birdSprites
+
+	dog := Dog{X: 100, Y: 150, Vy: 0}
+	bones := make([]Bone, 0)
+	score := 0
+
+	bgPic, _ := loadPicture("im.jpg")
+	bgSprite := pixel.NewSprite(bgPic, bgPic.Bounds())
+
+	bonePic, _ := loadPicture("bones.png")
+	boneSprite := pixel.NewSprite(bonePic, bonePic.Bounds())
+
+	spriteMap := make(map[string]*pixel.Sprite)
+
+	spriteFiles := []string{"dog1", "dog2", "dog3", "dog4", "dog5", "dog6", "dog7", "dog8"}
+	for _, fname := range spriteFiles {
+		pic, _ := loadPicture(fname + ".png")
+		spriteMap[fname] = pixel.NewSprite(pic, pic.Bounds())
+	}
+
+	setDogSprite(&dog, spriteMap, "NeutralRight")
+
+	atlas := text.NewAtlas(basicfont.Face7x13, text.ASCII)
+
+	// Goroutine 1
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		gravity := -0.5
+		for !win.Closed() {
+			mu.Lock()
+			tiempoTranscurrido++
+
+			frameCounter++
+			if frameCounter >= 10 { // Cambiar el sprite cada 10 frames
+				walkingFrame = 3 - walkingFrame // Cambia entre 1 y 2
+				frameCounter = 0
+			}
+
+			if tiempoTranscurrido >= tiempoMaximo {
+				gameEnded = true
+				winMessageShown = true
+			}
+
+			if !gameEnded {
+				if dog.Y <= 150 || (dog.Y <= 350 && dog.Y >= 300 && dog.X >= 300 && dog.X <= 500) {
+					if win.JustPressed(pixelgl.KeySpace) {
+						dog.Vy = 15
+					}
+				}
+
+				dog.Y += dog.Vy
+				if dog.Y > 150 {
+					dog.Vy += gravity
+				} else {
+					dog.Y = 150
+					dog.Vy = 0
+				}
+
+				toRemove := -1
+				for i, bone := range bones {
+					if (bone.X-dog.X)*(bone.X-dog.X)+(bone.Y-dog.Y)*(bone.Y-dog.Y) < 2500 {
+						score++
+						toRemove = i
+						break
+					}
+				}
+
+				if toRemove != -1 {
+					bones = append(bones[:toRemove], bones[toRemove+1:]...)
+				}
+
+				if score >= 20 && tiempoTranscurrido < tiempoMaximo {
+					// El jugador ha ganado
+					gameEnded = true
+					gameWon = true
+					winMessageShown = true
+				}
+
+			} else {
+				if win.JustPressed(pixelgl.KeyR) {
+					// Reiniciar el juego
+					gameEnded = false
+					gameWon = false
+					score = 0
+					dog.X = 100
+					dog.Y = 150
+					bones = nil
+					winMessageShown = false
+					tiempoTranscurrido = 0
+				}
+			}
+
+			mu.Unlock()
+			time.Sleep(time.Millisecond * 16)
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for !win.Closed() {
+			mu.Lock()
+			if !gameEnded {
+				if win.Pressed(pixelgl.KeyLeft) && dog.X >= 25 {
+					dog.X -= 10
+				}
+				if win.Pressed(pixelgl.KeyRight) && dog.X <= 775 {
+					dog.X += 10
+				}
+			}
+			mu.Unlock()
+			time.Sleep(time.Millisecond * 16)
+		}
+	}()
+
+	// Goroutine 3
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for !win.Closed() {
+			win.Clear(colornames.Greenyellow)
+
+			bgSprite.Draw(win, pixel.IM.Scaled(pixel.ZV, 0.8).Moved(win.Bounds().Center()))
+
+			mu.Lock()
+
+			frameCounter++
+			if frameCounter >= 10 { // Cambiar el sprite cada 10 frames
+				walkingFrame = 3 - walkingFrame // Cambia entre 1 y 2
+				frameCounter = 0
+			}
+
+			if dog.Vy != 0 {
+				if dog.X > 100 {
+					setDogSprite(&dog, spriteMap, "JumpingRight")
+				} else {
+					setDogSprite(&dog, spriteMap, "JumpingLeft")
+				}
+			} else if win.Pressed(pixelgl.KeyRight) {
+				setDogSprite(&dog, spriteMap, fmt.Sprintf("Right%d", walkingFrame))
+			} else if win.Pressed(pixelgl.KeyLeft) {
+				setDogSprite(&dog, spriteMap, fmt.Sprintf("Left%d", walkingFrame))
+			} else {
+				setDogSprite(&dog, spriteMap, "NeutralRight")
+			}
+
+			for _, bone := range bones {
+				boneSprite.Draw(win, pixel.IM.Scaled(pixel.ZV, 0.2).Moved(pixel.V(bone.X, bone.Y)))
+			}
+
+			dog.Sprite.Draw(win, pixel.IM.Scaled(pixel.ZV, 0.2).Moved(pixel.V(dog.X, dog.Y)))
+
+			txt := text.New(pixel.V(50, 550), atlas)
+			txt.Color = colornames.Black
+			fmt.Fprintf(txt, "Huesos obtenidos: %d", score)
+
+			if gameEnded {
+				// Mostrar mensaje de victoria o derrota y cómo reiniciar
+				bigTxt := text.New(pixel.V(150, 300), atlas)
+				bigTxt.Color = colornames.Black
+				bigTxt.Draw(win, pixel.IM.Scaled(bigTxt.Orig, 4)) // Escalar el texto 4 veces
+				if gameWon {
+					fmt.Fprintln(bigTxt, "¡Has ganado! Presiona R para reiniciar el juego")
+				} else {
+					fmt.Fprintln(bigTxt, "Has perdido, presiona R para reiniciar el juego")
+				}
+				bigTxt.Draw(win, pixel.IM.Scaled(bigTxt.Orig.Add(pixel.V(0, -100)), 2)) // Escalar el texto 2 veces
+			} else {
+				// Mostrar la cantidad de huesos obtenidos en la parte superior de la pantalla
+				txt := text.New(pixel.V(150, 530), atlas)
+				txt.Color = colornames.Black
+				fmt.Fprintf(txt, "Come 20 huesos para ganar! ")
+				fmt.Fprintf(txt, "Huesos obtenidos: %d", score)
+				txt.Draw(win, pixel.IM)
+			}
+
+			// Mostrar el tiempo transcurrido dentro de la imagen de fondo
+			txt.Clear() // Limpiar el contenido anterior de txt
+			txt.Color = colornames.Black
+			fmt.Fprintf(txt, "Tiempo restante: %d segundos", tiempoMaximo-tiempoTranscurrido)
+			txt.Draw(win, pixel.IM)
+
+			mu.Unlock()
+
+			win.Update()
+			time.Sleep(time.Millisecond * 16)
+		}
+	}()
+
+	// Goroutine para el pájaro
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for !win.Closed() {
+			mu.Lock()
+
+			// Cambia la posición del pájaro en la dirección actual
+			bird.X += 0.03 * float64(bird.Direction)
+			// Movimiento más lento
+			// Movimiento más lento
+
+			// Ajusta la velocidad de movimiento
+
+			// Cambia la dirección cuando el pájaro llega a los límites de la pantalla
+			if bird.X >= win.Bounds().Max.X || bird.X <= 0 {
+				bird.Direction *= -1
+			}
+
+			// Cambia el frame del pájaro si es necesario
+			bird.Frame++
+			if bird.Frame >= 10 {
+				bird.CurrentPic = (bird.CurrentPic + 1) % len(bird.Frames)
+				bird.Frame = 0
+			}
+
+			// Dibuja el pájaro en la pantalla
+			bird.Frames[bird.CurrentPic].Draw(win, pixel.IM.Scaled(pixel.ZV, 0.2).Moved(pixel.V(bird.X, bird.Y)))
+
+			mu.Unlock()
+
+		}
+	}()
+
+	// Goroutine 4
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for !win.Closed() {
+			mu.Lock()
+			if !gameEnded && len(bones) < 10 {
+				bone := Bone{X: rand.Float64()*150 + 125, Y: rand.Float64()*25 + 175}
+				if rand.Float64() > 0.5 {
+					bone.X = rand.Float64()*150 + 525
+				}
+				if rand.Float64() > 0.7 {
+					bone.X = rand.Float64()*150 + 325
+					bone.Y = rand.Float64()*25 + 375
+				}
+				bones = append(bones, bone)
+			}
+			mu.Unlock()
+			time.Sleep(time.Second)
+		}
+	}()
+
+	wg.Wait()
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"mydoggame/Models/scenes"
+
+	"github.com/faiface/pixel/pixelgl"
+)
+func main() {
+	pixelgl.Run(scenes.Aa)
+	//scenes.Aa()
+}
